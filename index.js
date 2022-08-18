52ce3ee10119fd27678bee0ef7ce407eb48154c0
Kaio Silveira <silveira.kaio@icloud.com>
2022-08-18 11:43:01 +0100

    move recordDueDate out of printOwing

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ function printOwing(invoice) {
   const outstanding = calculateOutstanding(invoice);
   recordDueDate(invoice);
   printDetails(invoice, outstanding);
+}
 
-  function recordDueDate(invoice) {
-    const today = Clock.today;
-    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
-  }
+function recordDueDate(invoice) {
+  const today = Clock.today;
+  invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
 }
 
 function printDetails(invoice, outstanding) {
