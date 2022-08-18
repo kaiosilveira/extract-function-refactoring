e52e86df65bf3f5c9d89cf3908da924b2708acef
Kaio Silveira <silveira.kaio@icloud.com>
2022-08-18 10:58:20 +0100

    move printDetails out of printOwing

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ function printOwing(invoice) {
     const today = Clock.today;
     invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
   }
+}
 
-  function printDetails(invoice, outstanding) {
-    console.log(`name: ${invoice.customer}`);
-    console.log(`amount: ${outstanding}`);
-    console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
-  }
+function printDetails(invoice, outstanding) {
+  console.log(`name: ${invoice.customer}`);
+  console.log(`amount: ${outstanding}`);
+  console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
 }
 
 function printBanner() {
