2c63868b10d7cf5fcbde0054c53b344651f47467
Kaio Silveira <silveira.kaio@icloud.com>
2022-08-18 08:38:03 +0100

    pass down printDetails dependencies as parameters

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ function printOwing(invoice) {
   const today = Clock.today;
   invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
 
-  printDetails();
+  printDetails(invoice, outstanding);
 
-  function printDetails() {
+  function printDetails(invoice, outstanding) {
     console.log(`name: ${invoice.customer}`);
     console.log(`amount: ${outstanding}`);
     console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
