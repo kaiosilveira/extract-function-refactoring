ba85fa8258abd98391edb80f3b8a92d4a6f4b1c4
Kaio Silveira <silveira.kaio@icloud.com>
2022-08-18 08:34:02 +0100

    replace details code block with function

--- index.js
+++ index.js
@@ -14,10 +14,7 @@ function printOwing(invoice) {
   const today = Clock.today;
   invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
 
-  // print details
-  console.log(`name: ${invoice.customer}`);
-  console.log(`amount: ${outstanding}`);
-  console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
+  printDetails();
 
   function printDetails() {
     console.log(`name: ${invoice.customer}`);
